5b7c94cf14720a4bfcb5dc034bcd659c2a4121a2
Simon <simon.j.goring@gmail.com>
2026-08-10 17:47:11 -0700

    Added sample function to the Python project.

---
+++ template/{% if primary_language == 'python' %}src{% endif %}/sample_function.py
@@ -0,0 +1,13 @@
+def sample_function(x:int) -> bool:
+    """_A sample function._
+
+    Args:
+        x (int): _An integer of your choosing._
+
+    Returns:
+        bool: _Returns True if you pass 1, otherwise returns False._
+    """
+    if x == 1:
+        return True
+    else:
+        return False
